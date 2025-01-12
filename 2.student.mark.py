cd9f045927f8ee677f91006477863d3d8964f6eb
Phuoc_Ngo <khongbietchu0123@gmail.com>
2025-01-12 11:03:22 +0000

    Create class Course

--- 2.student.mark.py
+++ 2.student.mark.py
@@ -4,4 +4,11 @@ class Student:
         self.name = name
         self.dob = dob
 
+class Course:
+    def __init__(self, course_id, name):
+        self.course_id = course_id
+        self.name = name
+        self.marks = {}
+
+
 
