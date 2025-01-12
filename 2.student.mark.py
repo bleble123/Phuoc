879c8a7c76c11c81d14684f58e4c9bfed390eac1
Ngo Phuoc <PhuocNM.23BI14361@usth.edu.vn>
2025-01-12 11:38:14 +0000

    class Mark

--- 2.student.mark.py
+++ 2.student.mark.py
@@ -30,6 +30,54 @@ class Course:
     def display(self):
         print(f"ID: {self.__course_id}, Name: {self.__name}")
 
+class MarkManager:
+    def __init__(self):
+        self.__marks = {}
 
-   
+    def input_mark(self, students, courses):
+        course_id = input("Enter the course ID to input marks: ")
+
+        course = None
+        for c in courses:
+            if c.get_id() == course_id:
+                course = c
+                break
+
+        if not course:
+            print("Course not found.")
+            return
+        
+        for student in students:
+            while True:
+                try:
+                    mark = float(input(f"Enter the mark for {student.get_name()} (ID: {student.get_id()}): "))
+
+                    if 0 <= mark <= 100:
+                        if student.get_id() not in self.__marks:
+                            self.__marks[student.get_id()] = {}
+                        self.__marks[student.get_id()][course_id] = mark
+                        break
+                    else:
+                        print("Invalid mark.")
+                except ValueError:
+                    print("Invalid input.")
+
+    def show_marks(self, students, courses):
+        course_id = input("Enter the course ID to show marks: ")
+
+        course = None
+        for c in courses:
+            if c.get_id() == course_id:
+                course = c
+                break
+
+        if not course:
+            print("Course not found.")
+            return
+
+        print("Marks for couse:")
+        for student in students:
+            mark = self.__marks.get(student.get_id(), {}).get(course_id, "No mark available")
+            print(f"{student.get_name()} (ID: {student.get_id()}): {mark}")
+
 
